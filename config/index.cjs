5bfcee02683bc783a2a64db0cf3fa59e706bcf4e
shenfenzhenghao <116780193+shenfenzhenghao@users.noreply.github.com>
2022-10-27 16:10:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otHap6EwzN0BZCgVg3vvSpj6tUwM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	JXNOtvrvmVSa6A2Kg-bXlhJ5hjseBWoUxL_l0aomQKs',
+      useTemplateId: 'WJ0ivNaMF0263YrnRCh48vnpIxdyFpUBSVoQYpuTPmc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-02',
       festivals: [
